376edffc37bd2e2a09d7aa599518c92659601911
wongpiwat <wongpiwat.san@gmail.com>
2024-12-22 21:04:38 -0500

    implement nextui to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,91 @@
+"use client";
+import { Chip } from "@nextui-org/react";
+import { MdAddCircleOutline } from "react-icons/md";
+import { MOCK_TASKS } from "@/constants/mock";
+
 import Button from "@/components/button/Button";
+import TaskCard from "@/components/card/TaskCard";
+import Rocket from "@/components/icon/Rocket";
 
 export default function Home() {
+  const handleCreateTask = () => {
+    console.log("Create Task");
+  };
+
+  const handleSelectTask = (id: string) => {
+    console.log(`Selected Task: ${id}`);
+  };
+
+  const handleDeleteTask = (id: string) => {
+    console.log(`Deleted Task: ${id}`);
+  };
+
   return (
     <div className="flex min-h-screen justify-center pt-16">
       <main className="flex w-1/2 flex-col gap-8">
         <div className="flex items-center justify-center">
+          <Rocket />
           <div className="text-2xl font-bold">Todo App</div>
         </div>
 
-        <Button>Create Task</Button>
+        <Button
+          onClick={handleCreateTask}
+          endComponent={<MdAddCircleOutline size={18} />}
+        >
+          Create Task
+        </Button>
 
         <div className="flex flex-row justify-between">
-          <div className="text text-primary-light font-bold">Tasks</div>
-          <div className="text text-secondary-light font-bold">Completed</div>
+          <div className="flex flex-row gap-1">
+            <div className="text font-bold text-primary-light">Tasks</div>
+            <Chip
+              size="sm"
+              classNames={{
+                base: "bg-gray-400 px-1.5",
+                content: "text-gray-200 font-bold",
+              }}
+            >
+              {MOCK_TASKS.length}
+            </Chip>
+          </div>
+
+          <div className="flex flex-row gap-1">
+            <div className="text font-bold text-secondary-light">Completed</div>
+            <Chip
+              size="sm"
+              classNames={{
+                base: "bg-gray-400 px-1.5",
+                content: "text-gray-200 font-bold",
+              }}
+            >
+              {MOCK_TASKS.filter((task) => task.status).length} of{" "}
+              {MOCK_TASKS.length}
+            </Chip>
+          </div>
         </div>
 
         <div className="flex flex-col items-center justify-center gap-2">
-          <div className="text font-bold text-gray-300">
-            You don&#39;t have any tasks registered yet.
-          </div>
-          <div className="text font-normal text-gray-300">
-            Create tasks and organize your to-do items.
-          </div>
+          {MOCK_TASKS.length > 0 ? (
+            MOCK_TASKS.map((task) => (
+              <TaskCard
+                key={task.id}
+                id={task.id}
+                title={task.title}
+                status={task.status}
+                onSelect={handleSelectTask}
+                onDelete={handleDeleteTask}
+              />
+            ))
+          ) : (
+            <>
+              <div className="text font-bold text-gray-300">
+                You don&#39;t have any tasks registered yet.
+              </div>
+              <div className="text font-normal text-gray-300">
+                Create tasks and organize your to-do items.
+              </div>
+            </>
+          )}
         </div>
       </main>
     </div>
---
+++ src/constants/mock.ts
@@ -0,0 +1,37 @@
+export const MOCK_TASKS = [
+  {
+    id: "1",
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    status: false,
+    timestamps: new Date(),
+  },
+  {
+    id: "2",
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    status: false,
+    timestamps: new Date(),
+  },
+  {
+    id: "3",
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    status: false,
+    timestamps: new Date(),
+  },
+  {
+    id: "4",
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    status: true,
+    timestamps: new Date(),
+  },
+  {
+    id: "5",
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    status: true,
+    timestamps: new Date(),
+  },
+];
