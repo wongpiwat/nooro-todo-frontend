08865e24227288469932fd2faa899dcd3a3280ce
wongpiwat <wongpiwat.san@gmail.com>
2024-12-23 22:24:19 -0500

    create confirmation modal

---
+++ src/components/modal/ConfirmationModal.tsx
@@ -0,0 +1,56 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from "@nextui-org/react";
+
+interface ConfirmationModalProps {
+  isOpen: boolean;
+  Title: string;
+  description: string;
+  leftLabel: string;
+  rightLabel: string;
+  onPress: () => void;
+  onClose: () => void;
+}
+
+const ConfirmationModal = ({
+  isOpen,
+  Title,
+  description,
+  leftLabel,
+  rightLabel,
+  onPress,
+  onClose,
+}: ConfirmationModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      classNames={{
+        base: "bg-gray-500",
+      }}
+    >
+      <ModalContent>
+        {() => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">{Title}</ModalHeader>
+            <ModalBody>{description}</ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                {leftLabel}
+              </Button>
+              <Button color="primary" onPress={onPress}>
+                {rightLabel}
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
