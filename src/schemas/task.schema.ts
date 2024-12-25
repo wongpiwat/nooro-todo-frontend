dfb75118d5c0c43866fcd3ac6f91979ff1c8e370
wongpiwat <wongpiwat.san@gmail.com>
2024-12-24 21:02:38 -0500

    set limit to title text field

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -2,7 +2,10 @@ import { z as zod } from "zod";
 
 export const schema = zod.object({
   id: zod.string().min(1, { message: "Id is required" }),
-  title: zod.string().min(1, { message: "Title is required" }),
+  title: zod
+    .string()
+    .min(1, { message: "Title is required" })
+    .max(160, { message: "Title is over 160 characters" }),
   color: zod.string().min(1, { message: "Color is required" }),
   status: zod.boolean({ message: "Status is required" }),
   timestamps: zod.date({ message: "Timestamps is required" }),
